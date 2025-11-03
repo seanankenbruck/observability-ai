c0f02acdced687b394c1dbbbc0a4dc165d12dc49
Sean Ankenbruck <sean.ankenbruck@gmail.com>
2025-11-03 16:05:26 -0500

    fix missing readme file and resolve test issues

--- internal/mimir/client.go
+++ internal/mimir/client.go
@@ -1,4 +1,15 @@
 // internal/mimir/client.go
+package mimir
+
+import "time"
+
+// AuthConfig holds authentication configuration for Mimir
+type AuthConfig struct {
+	Username string
+	Password string
+	Token    string
+}
+
 type Client struct {
 	endpoint string
 	auth     AuthConfig
